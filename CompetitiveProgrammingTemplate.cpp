924a26dd60e496ff681f16501e0c3fc25443b9af
Satvik <7vikpandey@gmail.com>
2024-01-04 02:11:44 +0530

    Update CompetitiveProgrammingTemplate.cpp

--- CompetitiveProgrammingTemplate.cpp
+++ CompetitiveProgrammingTemplate.cpp
@@ -1,5 +1,12 @@
 #include <bits/stdc++.h> 
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
+
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
  
 typedef long long ll;
 typedef long double ld;
